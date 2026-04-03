b0df3419804579397fc95bc726e7aba5c010f9f6
antfullstack <253273528+antfullstack@users.noreply.github.com>
2026-04-04 01:27:30 +0300

    Added percentage function

--- simple_equ/economics/statistics.py
+++ simple_equ/economics/statistics.py
@@ -1,18 +1,18 @@
-def average_mean(lst):
+def average(lst):
     # Converts any numbers in string form to ints and checks for invalid inputs
     try:
         lst = list(map(int, lst))
     except ValueError:
-        print("Invalid integer in list.")
+        print("Invalid argument types.")
     
     num_sum = sum(lst)
     return num_sum / len(lst)
 
-def average_median(lst):
+def median(lst):
     try:
         lst = list(map(int, lst))
     except ValueError:
-        print("Invalid integer in list.")
+        print("Invalid argument types.")
     
     sorted_list = sorted(lst)
     median_index = (len(sorted_list) - 1) // 2
@@ -21,4 +21,9 @@ def average_median(lst):
         return (sorted_list[median_index] + sorted_list[median_index + 1]) / 2
     return sorted_list[median_index]
     
-    
+def percentage(fraction, whole):
+    try: 
+        result = fraction / whole * 100
+        return result
+    except ValueError:
+        print("Invalid argument types.")
